7e73e97704a8d2d90e5f32d918b63f9f0555b194
Jarod Luebbert <jarod.luebbert@gmail.com>
2010-04-19 22:51:03 -0500

    Added makefile and started creating the threads.

--- Program03/scheduler.c
+++ Program03/scheduler.c
@@ -19,6 +19,59 @@
 #include <errno.h>
 #include <pthread.h>
 
+#define NTHREADS 5
+#define NUM_LOOPS 1000000
+
+void *child_thread(void *arg);
+
 int main(void) {
+    // holds the ids for each thread
+    int ids[NTHREADS];
+
+    // holds the system assigned thread ids
+    pthread_t threads[NTHREADS];
+
+    // init thread scheduler vector
+    int schedule_vector[NTHREADS];
+    // give each thread their priority
+    schedule_vector[0] = 0;
+    schedule_vector[1] = 2;
+    schedule_vector[2] = 4;
+    schedule_vector[3] = 1;
+    schedule_vector[4] = 3;
+
+    int i;
+    // create the child threads
+    for (i = 0; i < NTHREADS; i++) {
+        // set the thread id and pass it as the argument
+        ids[i] = i;
+
+        // create the thread
+        pthread_create(&threads[i], NULL, child_thread, &ids[i]);
+    }
+
+    // start the scheduler
+    while (1) {
+        ;
+    }
+
     return 0;
+}
+
+void *child_thread(void *arg) {
+    long int i;
+    long int counter;
+    int myid = *(int*) arg;
+
+    counter = 0;
+
+    for (;;) {
+        printf("T: %d runs -t\t\n", myid);
+
+        for (i = 0; i < NUM_LOOPS; i++)
+            counter += 1;
+
+        if (counter == 500000)
+            counter = 0;
+    }
 }
