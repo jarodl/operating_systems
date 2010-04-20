2908a322f1f2a2d8983e3f528e9436a5f22d9986
Jarod Luebbert <jarod.luebbert@gmail.com>
2010-04-19 21:50:47 -0500

    Starting project 3.

---
+++ Program03/scheduler.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  scheduler.c
+ *
+ *    Description:  A pre-emptive user mode thread scheduler.
+ *
+ *        Version:  1.0
+ *        Created:  04/19/2010 21:47:34
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jarod Luebbert
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+
+int main(void) {
+    return 0;
+}
